8ef7f2f2b317d56c8f7c2a3c49de23edf6971940
bbajra <aa7174sa@go.metrostate.edu>
2018-04-01 11:32:25 -0500

    Changes for iteration3
    Added 4 different pages for 4 different maps and changed some code in index.php from previous versions. Also added 4 different geojson layer in data folder for the 4 different maps that we are trying to display

--- src/Gerrymanderin_Site/index.php
+++ src/Gerrymanderin_Site/index.php
@@ -1,71 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<!link rel="stylesheet" href="lib/leaflet/leaflet.css">
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>
-	 <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
-	
-	<!script src="lib/leaflet/leaflet.js"></script>
-	
-	<script src="data/countries.geojson"></script>
-	<script src="data/mnprecinctparty.geojson"></script>
-	<script src="data/mnprecinctparty2.geojson"></script>
-	<style type="text/css">
-			#map { height : 720px; width : 1200px;}
-			.info { padding: 6px 8px; font: 14px/16px Arial, Helvetica, sans-serif; background: white; background: rgba(255,255,255,0.8); box-shadow: 0 0 15px rgba(0,0,0,0.2); border-radius: 5px; } .info h4 { margin: 0 0 5px; color: #777; }
-			.legend { text-align: left; line-height: 18px; color: #555; } .legend i { width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7; }
-	</style>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!link rel="stylesheet" href="lib/leaflet/leaflet.css">
+		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>
+		<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
 		
-	<?php
-	/*    Using "mysqli" instead of "mysql" that is obsolete.
-	* Change the value of parameter 3 if you have set a password on the root userid
-	* Add port number 3307 in parameter number 5 to use MariaDB instead of MySQL
-	*
-	*     Utilisation de "mysqli" à la place de "mysql" qui est obsolète.
-	* Changer la valeur du 3e paramètre si vous avez mis un mot de passe à root
-	* Ajouter le port 3307 en paramètre 5 si vous voulez utiliser MariaDB
-	* Use your USER NAME and PASSWORD
-	*/
-	$mysqli = new mysqli('127.0.0.1', 'root', '');
-
-	if ($mysqli->connect_error) {
-		die('Connect Error (' . $mysqli->connect_errno . ') '
-				. $mysqli->connect_error);
-	}
-	
-	/* 
-	* Database Queries
-	*/
-	/*
-	$sql ="select pctname ,USPRSR, USPRsDFL, usprstotal from ics499.results";
-	$result = $mysqli->query($sql);
-	
-	if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-         echo "<br> Precint name: ". $row["pctname"]. " - Preidential Republican Votes: ". $row["USPRSR"]. " Presidental Democrat Votes: " . $row["USPRsDFL"] . " Total Number of Voters: ". $row["usprstotal"]. "<br>";
-    }
-	} else {
-		echo "0 results";
-	}*/
-	
-	echo '<p>Connection OK '. $mysqli->host_info.'</p>';
-	echo '<p>Server '.$mysqli->server_info.'</p>';
-	$mysqli->close();
-	?>
+		<!script src="lib/leaflet/leaflet.js"></script>
+		
+		<script src="data/mnprecinctparty.geojson"></script>
+		<style type="text/css">
+				#map { height : 720px; width : 1200px;}
+				.info { padding: 6px 8px; font: 14px/16px Arial, Helvetica, sans-serif; background: white; background: rgba(255,255,255,0.8); box-shadow: 0 0 15px rgba(0,0,0,0.2); border-radius: 5px; } .info h4 { margin: 0 0 5px; color: #777; }
+				.legend { text-align: left; line-height: 18px; color: #555; } .legend i { width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7; }
+		</style>
 	</head>
 	<title>
-	Gerrymanderin interactive map
+	Interactive map
 	</title>
-	<body>
+	<body style="background-color:#ffffff;">
 	<nav class="navbar navbar-inverse">
 	  <div class="container-fluid">
-		<div class="navbar-header">Gerrymanderin</div>
 		<ul class="nav navbar-nav">
 		  <li class="active"><a href="#">Home</a></li>
 		  <li><a href="#">Recommendation</a></li>
@@ -77,45 +36,42 @@
 		</form>
 	  </div>
 	</nav>
-	<div class="jumbotron text-center" style="background-color:#ebf0fa;">
-	  <h1>Gerrymanderin Project</h1>
+	<div class="jumbotron text-center"  style="background-color:#f0f0f0;">
+	  <h1>Gerrymandering Project</h1>
 	</div>
-	<div class="btn-group btn-group-lg">
-	<button type="button" class="btn btn-primary"><span class="glyphicons glyphicons-map"></span> Map </button>
-	<button type="button" class="btn btn-primary"><span class="glyphicons glyphicons-pie-chart"></span>Chart</button>
-	<button type="button" class="btn btn-info">Info</button>
-	<a href="#" class="btn btn-success btn-lg">
-      <span class="glyphicon glyphicon-print"></span> Print 
-    </a>
+	<div align="center">Click on the buttons below to view the election results</div>
+	<br>
+	<div align="center">
+	<a target="_blank" href="usCongress.html" class="btn btn-default"> U.S. Congress </a>
+	<a target="_blank" href="presidential.html" class="btn btn-default">Presidential</a>
+	<a target="_blank" href="mnSenate.html" class="btn btn-default">MN Senate</a>
+	<a target="_blank" href="mnHouse.html" class="btn btn-default"> MN House </a>
 	</div>
 	
 	<div class="container">
-	<h1>World Map</h1>
+	<h1>Map of Minnesota</h1>
 		<div id="map"></div>
 		<script>
+		/**
+		* For GeoJson reference-properties: CountyID
+		*/
 		//Initializing the map
 			var mapboxAccessToken = 'pk.eyJ1IjoiYmJhanJhIiwiYSI6ImNqY3Q2eDYycTBmZ3kyeHZ0ajZsNGtvajIifQ.tKzBEEfc7mCC0HmVy-KYuw';
-			var map = L.map('map').setView([44.9375, -93.2010],2);
+			var map = L.map('map').setView([44.9375, -93.2010],14);
 		
 		//Loading the title layer
-			/*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
-				id: 'mapbox.light',
-				attribution: 'State map data',
-			}).addTo(map);*/
 			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-				maxZoom: 90,
+				//maxZoom: 90,
 				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 					'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 					'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 				id: 'mapbox.light'
 			}).addTo(map);
-			
-		//Loading geoJson from an external file
-			var precinctLayer = L.geoJson(mn).addTo(map);
-			map.fitBounds(precinctLayer.getBounds());
-			var partyelectionLayer = L.geoJson(minnesota).addTo(map);
-			map.fitBounds(partyelectionLayer.getBounds());
 		
+		//Loading geoJson from an external file
+			var precinctLayer = L.geoJson(minnesota).addTo(map);
+			map.fitBounds(precinctLayer.getBounds());
+			
 		// control that shows map information from GeoJson file when hovering
 		var mapLayerInformation = L.control();
 
@@ -126,8 +82,8 @@
 		};
 
 		mapLayerInformation.update = function (props) {
-			this._div.innerHTML = '<h4>Election Results Precinct</h4>' +  (props ?
-				'<b>' + 'District:' + props.CongDist + '</b><br />' + 'County: ' + props.County + '</b><br/>' + 'Winning Party: ' + props.party
+			this._div.innerHTML = '<h4>Minnesota County</h4>' +  (props ?
+				'<b>' + 'County:' + props.CountyID + '</b><br />' + 'County: ' + props.County + '</b><br/>'
 				: 'Hover over an area');
 		};
 
@@ -139,28 +95,28 @@
 		*/
 
 		function getColor(district){
-				return district==8 ? '#800026' :
-						district==7 ? '#BD0026' :
-						district==6 ? '#FC4E2A' :
-						district==5 ? '#E31A1C' :
-						district==4 ? '#FD8D3C' :
-						district==3 ? '#2b8cbe' :
-						district==2 ? '#2ca25f' :
-						district==1 ? '#FFEDA0' :
-							   '#ff0000' ;
+			var purple = ["#f7fcfd","#e0ecf4","#bfd3e6","#9ebcda","#8c96c6","#8c6bb1","#88419d","#810f7c","#4d004b"];
+				return district % 10 ==1  ? purple[0] :
+						district % 10 ==2 ? purple[1] :
+						district % 10 ==3 ? purple[2] :
+						district % 10 ==4 ? purple[3] :
+						district % 10 ==5 ? purple[4] :
+						district % 10 ==6 ? purple[5] :
+						district % 10 ==7 ? purple[6] :
+						district % 10 ==8 ? purple[7] :
+										   purple[8] ;
 		}
 							
 		function style(feature) {
 			return {
-				fillColor: getColor(feature.properties.CongDist),
-				weight: 1,
+				fillColor: getColor(feature.properties.CountyID),
+				weight: 0.5,
 				opacity: 1,
 				color: 'black',
-				dashArray: '0.5',
+				dashArray: '0.3',
 				fillOpacity: 0.7				
 			};
 		}
-
 		/*
 		* Adding interaction to map with hovering and zooming feature
 		*/
@@ -200,53 +156,14 @@
 				click: zoomToFeature
 			});
 		}
-		
 		/*
 		* Adding two layers to the map
 		*/
 
-		var colorLayer = L.geoJson(mn, {
+		var colorLayer = L.geoJson(minnesota, {
 			style: style,
 			onEachFeature: onEachFeature
 			}).addTo(map);
-		
-		function secondLayerFeature(feature, mapLayer) {
-			mapLayer.on({
-				mouseover: mouseoverSecondLayer
-			});
-		}
-		function mouseoverSecondLayer(e) {
-			var mapSecondLayer = e.target;
-
-			if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-				mapSecondLayer.bringToFront();
-			}
-
-			mapLayerInformation.update(mapSecondLayer.feature.properties);
-		}
-
-		var partyLayer = L.geoJson(minnesota, {
-			style: function(feature) {
-					switch (feature.properties.party) {
-						case 'Republican': return {color: "#ff0000"};
-						case 'Democrat':   return {color: "#0000ff"};
-					}
-			},
-			weight: 1,
-			opacity: 1,
-			color: '',
-			dashArray: '0.5',
-			fillOpacity: 0.7,
-			onEachFeature: secondLayerFeature
-			}).addTo(map);
-
-		var basemaps = {
-			"Congressional": colorLayer,
-			"Party": partyLayer
-		};
-
-
-		L.control.layers(basemaps, {}, {collapsed: false}).addTo(map);
 		</script>
 	</div>
 	
@@ -261,8 +178,8 @@
 		  <p>The use of interactive map helps to visualize the data in a convenient way.</p>
 		</div>
 		<div class="col-sm-4">
-		  <h3>Chart</h3>        
-		  <p>The chart form helps to further analize the data.</p>
+		  <h3>Districts</h3>        
+		  <p>Election results for each districts can be viewed by clicking on the respective buttons.</p>
 		</div>
 	  </div>
 	</div>
